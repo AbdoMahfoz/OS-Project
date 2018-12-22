f5134c7c8391fefb3bb2ea45a5a38cf66768b02d
Nour ElGafy <nour-elgafy@outlook.com>
2018-12-22 14:25:48 +0200

    tpr5 running

--- kern/trap.c
+++ kern/trap.c
@@ -436,16 +436,23 @@ void remove_pages(struct Env * e, uint32 PTBR, uint32 size) {
 		env_page_ws_clear_entry(e, j);
 	}
 }
-void page_fault_handler(struct Env * curenv, uint32 fault_va) {
+void FreeWs(struct Env * curenv, bool x) {
 	int size = env_page_ws_get_size(curenv);
-	if (size == curenv->page_WS_max_size) {
-		int NumberOfPagesToBeRemoved = ((curenv->page_WS_max_size
-				* curenv->percentage_of_WS_pages_to_be_removed) / 100)
-				+ (((curenv->page_WS_max_size
-						* curenv->percentage_of_WS_pages_to_be_removed) % 100)
-						!= 0);
+	if (x || size == curenv->page_WS_max_size) {
+		int NumberOfPagesToBeRemoved = (curenv->page_WS_max_size
+				* curenv->percentage_of_WS_pages_to_be_removed);
+		NumberOfPagesToBeRemoved = ROUNDUP(NumberOfPagesToBeRemoved, 100);
+		NumberOfPagesToBeRemoved = NumberOfPagesToBeRemoved / 100;
+		/*cprintf(" Number Of Pages To be Removed  '%d'\n ",
+		 NumberOfPagesToBeRemoved);
+		 cprintf(" Percentage '%d'\n ",
+		 curenv->percentage_of_WS_pages_to_be_removed);
+		 cprintf(" max size '%d'\n ", curenv->page_WS_max_size);
+		 */
 		int c = 0;
 		while (c < NumberOfPagesToBeRemoved) {
+			if (env_page_ws_get_size(curenv) <= c)
+				break;
 			int index = 0;
 			uint32 min = 0;
 			while (1) {
@@ -457,7 +464,7 @@ void page_fault_handler(struct Env * curenv, uint32 fault_va) {
 				}
 			}
 
-			for (int q = index + 1; q < curenv->page_WS_max_size; q++) {
+			for (int q = index; q < curenv->page_WS_max_size; q++) {
 				if (env_page_ws_is_entry_empty(curenv, q) == 1) {
 					continue;
 				} else {
@@ -471,7 +478,7 @@ void page_fault_handler(struct Env * curenv, uint32 fault_va) {
 			uint32* pgtable = NULL;
 			uint32 Victum = env_page_ws_get_virtual_address(curenv, index);
 			struct Frame_Info *ptr_frame_info = get_frame_info(
-							curenv->env_page_directory, (void*) Victum, &pgtable);
+					curenv->env_page_directory, (void*) Victum, &pgtable);
 			if ((pgtable[PTX(Victum)] & PERM_MODIFIED) != 0) {
 				//Update and UnMap
 
@@ -490,6 +497,78 @@ void page_fault_handler(struct Env * curenv, uint32 fault_va) {
 			c++;
 		}
 
+	}
+
+}
+void ClearWs(struct Env * curenv) {
+	int size = env_page_ws_get_size(curenv);
+	int index = 0;
+	int c = 0;
+	while (c <= size) {
+		if (size == 0 || index>=curenv->page_WS_max_size)
+			return;
+		while (1) {
+			if (env_page_ws_is_entry_empty(curenv, index) == 1) {
+				if (index < curenv->page_WS_max_size){
+					cprintf("empty Page \n");
+					index++;
+				}				else if (index >= curenv->page_WS_max_size){
+					return;
+				}
+				break;
+			} else {
+				break;
+			}
+		}
+		cprintf("size  = %d\n", size);
+		cprintf("index = %d\n", index);
+		cprintf("Cycle Counter = %d\n", c);
+		cprintf("Max Index = %d \n",curenv->page_WS_max_size);
+
+		uint32* pgtable = NULL;
+		uint32 Victum = env_page_ws_get_virtual_address(curenv, index);
+		struct Frame_Info *ptr_frame_info = get_frame_info(
+				curenv->env_page_directory, (void*) Victum, &pgtable);
+		if ((pgtable[PTX(Victum)] & PERM_MODIFIED) != 0) {
+			//Update and UnMap
+
+			int ret = pf_update_env_page(curenv, (void*) Victum,
+					ptr_frame_info);
+
+			if (ret != 0) {
+				panic("page_fault_handler() is not implemented yet...!!");
+			}
+
+			unmap_frame(curenv->env_page_directory, (void*) Victum);
+			env_page_ws_clear_entry(curenv, index);
+		} else {
+			unmap_frame(curenv->env_page_directory, (void*) Victum);
+			env_page_ws_clear_entry(curenv, index);
+		}
+		c++;
+		index++;
+	}
+}
+void page_fault_handler(struct Env * curenv, uint32 fault_va) {
+	uint32 FreeFrames = calculate_free_frames();
+	uint32 FramePercentage = ((FreeFrames / number_of_frames) * 100);
+	if (FramePercentage < memory_scarce_threshold_percentage) {
+		FreeWs(curenv, 1);
+		struct Env* TempEnv = NULL;
+		for (int q = 0; q < num_of_ready_queues; q++) {
+			LIST_FOREACH(TempEnv,& env_ready_queues[q])
+			{
+				FreeWs(TempEnv, 1);
+			}
+		}
+		LIST_FOREACH(TempEnv,&env_exit_queue)
+		{
+			//FreeWs(TempEnv, 1);
+			ClearWs(TempEnv);
+		}
+
+	} else {
+		FreeWs(curenv, 0);
 	}
 
 	//zizo
